b39bc65c70dad22c8fd03fc145c9fdfe6974ac1c
roroudev <rextechpos@gmail.com>
2026-04-08 18:31:21 +0800

    fix error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,6 +6,10 @@ declare global {
 
 let prisma: PrismaClient
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined in environment variables')
+}
+
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient({
     log: ['error', 'warn'],
